ad812aeca650167afe8227052d0908fb761b0a01
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2018-09-09 22:44:05 -0700

    UI Fixes.

--- dashboard/static/js/dashboard-main.js
+++ dashboard/static/js/dashboard-main.js
@@ -1,15 +1,23 @@
 function loadComponents(components) {
     components.forEach(function(component) {
-        return GenericApplication('.content .weave-small-cards-row', component);
+        return GenericApplication('.content .weave-medium-cards-row', component);
     });
 }
 
 $(document).ready(function() {
-    DEBUG_APPS = [];
+    Vue.config.errorHandler = function (msg, vm, trace) {
+      console.log(msg, trace);
+    }
+    Vue.config.warnHandler = function (msg, vm, trace) {
+      console.log(msg, trace);
+    }
     registerComponent('vertical-layout', '#template-vertical-layout');
     registerComponent('header-3', '#template-h3');
     registerComponent('paragraph', '#template-paragraph');
     registerComponent('weave-button', '#template-button');
+    registerComponent('medium-card', '#template-medium-card');
+    registerComponent('card-footer-status', '#template-card-footer-status');
+    registerComponent('weave-icon', '#template-weave-icon');
     $.ajax({
         url: "/api/status-cards",
         dataType: "json"
